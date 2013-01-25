bf1f8b6c8377fdf53170d0e218f7fe7582c79e0a
vanya-v <vanco2000@tut.by>
2013-01-25 18:48:08 +0300

    Added AppCode;

---
+++ AppCode/SuperOnlineShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SuperOnlineShop.Models;
+using Umbraco.Web.Mvc;
+
+namespace SuperOnlineShop.Controllers {
+    public class ShoppingCartController : Controller{//SurfaceController {
+        //
+        // GET: /ShoppnigCart/
+
+        public ActionResult Index() {
+            List<ShoppingCartItem> shoppingCartItems = GetItems();//for testing
+            ViewBag.TotalPrice = shoppingCartItems.Sum(item => item.Price*item.Count);
+            return View(shoppingCartItems);
+        }
+
+        public ActionResult RecountPrice(){
+            return Json(1340); //for testing
+        }
+
+        private List<ShoppingCartItem> GetItems(){
+            List<ShoppingCartItem> result = new List<ShoppingCartItem>();
+            result.Add(new ShoppingCartItem{Id=1, Name = "Nokia Lumia 920", Price=890, Count=1});
+            result.Add(new ShoppingCartItem{Id=2, Name = "Nokia Lumia 820", Price=690, Count=2});
+            return result;
+
+        }
+    }
+}
