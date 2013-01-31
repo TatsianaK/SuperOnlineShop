21cfbe80f53412d082714a12b4a7f921490d983b
vanya-v <vanco2000@tut.by>
2013-01-31 18:16:50 +0300

    Added Unit tests project;

---
+++ AppCode/SuperOnlineShop.Tests/ShoppingCartControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuperOnlineShop.Tests
+{
+    [TestClass]
+    public class ShoppingCartControllerTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.AreEqual(1, 1, "Test Message");
+        }
+    }
+}
