6639483fe299524e9d670f30f86af83779d5f4a6
Nastishka <anastasiya_matronkina@epam.com>
2013-01-31 18:34:14 +0300

    Added new markup for master page

---
+++ scripts/SearchScript.js
@@ -0,0 +1,21 @@
+﻿window.searchInputHelpText = ' Search entire store...';
+function searchTextInput_OnFocus(target) {
+    if (target.value == window.searchInputHelpText) {
+        target.value = '';
+    }
+    return true;
+};
+function searchTextInput_OnBlur(target) {
+    if ($.trim(target.value) == '') {
+        target.value = window.searchInputHelpText;
+    }
+    return true;
+};
+function submitButton_OnClick(event, target, value) {
+    event.preventDefault();
+    if (!($.trim(value) == '' || value == window.searchInputHelpText)) {
+        target.form.submit();
+    } else {
+        return false;
+    }
+};
