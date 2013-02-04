512db0166adf1e23da37fe200366fa63fc6b4474
TatsianaK <tatsiana_kazak@epam.com>
2013-02-04 17:01:15 +0300

    Added Unity, but not configured yet

---
+++ AppCode/SuperOnlineShop/Helpers/IShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SuperOnlineShop.Helpers {
+    public interface IShoppingCart {
+        Dictionary<int, int> GetItems(ControllerContext context);
+        void DeleteItem(ControllerContext context, int id);
+        void UpdateCartItems(ControllerContext context, Dictionary<int, int> items);
+        void AddItemsToCart(ControllerContext context, int id, int count);
+    }
+}
