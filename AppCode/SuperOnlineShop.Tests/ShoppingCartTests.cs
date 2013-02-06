89172f2a4ecf2365d82dd554e4d7c346b424c02e
TatsianaK <tatsiana_kazak@epam.com>
2013-02-06 14:40:13 +0300

    Added file for testing ShoppingCart class

---
+++ AppCode/SuperOnlineShop.Tests/ShoppingCartTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SuperOnlineShop.Controllers;
+using SuperOnlineShop.Helpers;
+
+namespace SuperOnlineShop.Tests {
+    [TestClass]
+    public class ShoppingCartTests {
+
+        [TestMethod]
+        public void MyTest() {
+            ShoppingCart cart = new ShoppingCart();
+
+            var context = new Mock<ControllerContext>();
+            var mockSession = new Mock<HttpSessionStateBase>();
+            context.Setup(p => p.HttpContext.Session).Returns(mockSession.Object);
+
+            //context.Object.HttpContext.Session["test"]=123;
+
+            cart.AddItemsToCart(context.Object, 1078, 1);
+            Dictionary<int, int> items = cart.GetItems(context.Object);
+            Assert.AreEqual(1,items.Count(), "Count is incorrect");
+        }
+
+    }
+}
