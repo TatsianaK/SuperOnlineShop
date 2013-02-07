4a379c58bc66d9accbcca3e1584a26cd1caebd6a
TatsianaK <tatsiana_kazak@epam.com>
2013-02-07 14:40:53 +0300

    Added comments page. New comments are not added yet.

--- AppCode/SuperOnlineShop/Controllers/CommentsController.cs
+++ AppCode/SuperOnlineShop/Controllers/CommentsController.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SuperOnlineShop.Models;
 using Umbraco.Web.Mvc;
 
-namespace SuperOnlineShop.Controllers
-{
-    public class CommentsController : SurfaceController
-    {
+namespace SuperOnlineShop.Controllers {
+    public class CommentsController : SurfaceController {
         //
         // GET: /Comments/
 
-        public ActionResult Index()
-        {
+        public ActionResult Index(int? id) {
+            List<Comment> result = new List<Comment>();
+            if (id.HasValue) {
+                List<Comment> comments = new List<Comment>();
+                comments.Add(new Comment { ProductId = 1078, UserName = "Tanya", Title = "Very good device", Text = "Cool device! Buy it!", SubmitDate = DateTime.Now });
+                comments.Add(new Comment { ProductId = 1079, UserName = "Vanya", Title = "Worth to try", Text = "Cool device! Buy it!", SubmitDate = DateTime.Now });
+                comments.Add(new Comment { ProductId = 1079, UserName = "Nastishka", Title = "Good device", Text = "Cool device!", SubmitDate = DateTime.Now });
+                comments.Add(new Comment { ProductId = 1078, UserName = "Tanya", Title = "Not bad", Text = "Not bad", SubmitDate = DateTime.Now });
+            
+                result = comments.Where(comment => comment.ProductId == id).ToList();
+                return View(result);
+            }
             return View();
+        }
+
+        public ActionResult AddComment(Comment comment) {
+            if(comment.ProductId!=null){
+                //add comment
+            }
+            return RedirectToAction("Index", new {id=comment.ProductId});
         }
 
     }
--- AppCode/SuperOnlineShop/Models/Comment.cs
+++ AppCode/SuperOnlineShop/Models/Comment.cs
@@ -6,7 +6,10 @@ using System.Web;
 namespace SuperOnlineShop.Models {
     public class Comment {
         public int Id { get; set;}
-        public int UserId {get; set;}
+        public int ProductId { get; set;}
+        public string UserName {get; set;}
+        public string Title {get; set; }
         public string Text { get; set;}
+        public DateTime SubmitDate {get; set;}
     }
 }
